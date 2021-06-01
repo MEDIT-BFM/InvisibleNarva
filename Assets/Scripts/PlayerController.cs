03f23d96519ee6aee7a493a04888626e021d2e03
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2021-06-01 18:21:46 +0300

    Imported TextMeshPro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,20 @@
 using UnityEngine;
-//using UnityStandardAssets.Characters.FirstPerson;
 
-public class PlayerController : MonoBehaviour
-{
- //   public FixedJoystick joystick;
- //   public FixedJoystick lookstick;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+    public FixedJoystick joystick;
+    public FixedJoystick lookstick;
 
-    //void Update () {
- //       var fps = GetComponent<RigidbodyFirstPersonController>();
- //       if (joystick.gameObject.transform.parent.gameObject.activeInHierarchy)
- //       {            
- //           fps.RunAxis = joystick.inputVector;
- //           fps.mouseLook.LookAxis = lookstick.inputVector;
- //       }
- //       else
- //       {
- //           joystick.OnPointerUp(null);
- //           lookstick.OnPointerUp(null);
- //           fps.RunAxis = Vector2.zero;
- //           fps.mouseLook.LookAxis = Vector2.zero;
- //       }     
- //   }
+    private float speed = 2f;
+    private Vector3 velocity;
+    private CharacterController characterController;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        velocity = new Vector3(0, 0, joystick.Vertical);
+        characterController.SimpleMove(speed * velocity);
+    }
 }
