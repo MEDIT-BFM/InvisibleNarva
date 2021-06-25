4ec02939e8252e58a87f5190adf60715e6988aab
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2021-06-25 15:56:37 +0300

    Added Building Message Logic When Player Enter Area

---
+++ Assets/Scripts/BuildingArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace InvisibleNarva {
+    public class BuildingArea : MonoBehaviour {
+        public static event Action<string> OnEnter = delegate { };
+
+        [SerializeField, TextArea(2, 4)] private string buildingName;
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag != "Player") {
+                return;
+            }
+
+            OnEnter?.Invoke(buildingName);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MessageUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+namespace InvisibleNarva {
+    public class MessageUI : MonoBehaviour {
+        [SerializeField] private float fadeDuration = 1.5f;
+        [SerializeField] private float displayDuration = 3;
+        [SerializeField] private RectTransform holder;
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private TextMeshProUGUI message;
+
+        private void OnEnable() {
+            BuildingArea.OnEnter += BuildingEnterHandler;
+            canvasGroup.alpha = 0;
+        }
+
+        private void BuildingEnterHandler(string buildingName) {
+            message.text = buildingName;
+          //message.SetDynamicText(holder, buildingName, Vector2.zero);
+            canvasGroup.DOFade(1, fadeDuration).OnComplete(() => canvasGroup.DOFade(0, fadeDuration).SetDelay(displayDuration));
+        }
+
+        private void OnDisable() {
+            BuildingArea.OnEnter -= BuildingEnterHandler;
+        }
+    }
+}
